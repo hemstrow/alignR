b716cee378097a491e5db12707dd1eb262a7087d
hemstrow <whemstrom@ucdavis.edu>
2022-08-02 18:09:51 -0600

    more work on shiny gui app

--- inst/shiny/ui_app/app.R
+++ inst/shiny/ui_app/app.R
@@ -1,118 +1,43 @@
 library(shiny)
+library(shinyjs)
+library(shinyFiles)
 source("app_code.R")
 
+root <- c(wd = normalizePath("."), root = "/")
+
 ui <- fluidPage(
+  useShinyjs(),
   
   #===============Demultiplexing and initial inputs===============
   titlePanel("Demultiplexing:"),
   sidebarLayout( 
     sidebarPanel(
-      fluidRow(selectInput("barcodes", "Where are your barcodes located?", choices = list("In Illumina Headers",
-                                                                                          "Start of Reads",
-                                                                                          "Seperate fastq File"),
+      fluidRow(selectizeInput("barcodes", 
+                              "Where are your barcodes located?", 
+                              choices = list("In Headers",
+                                             "Start of Reads",
+                                             "Seperate fastq File"), 
+                              options = list(maxItems = 2),
                            multiple = TRUE),
-               checkboxInput("paired_or_single", "Is your data paired-end?", value = FALSE),
-               actionButton("visualize_barcodes", "Click for example of selection"))
+               checkboxInput("is.paired", "Is your data paired-end?", value = FALSE),
+               actionButton("visualize_barcodes", "Visualize and proceed to file input"))
     ),
     mainPanel(
       uiOutput("barcode_locations_image"))
   ),
   
-  # # subset sidebar
-  # textOutput(outputId = "subset_header"),
-  # fluidRow(textInput(inputId = "subset", label = "Subsetting call, see ?subset_snpR_data for more info"),
-  #          actionButton("subset", "Subset Data")), # note that this should change the input_validation return!
-  # 
-  # #============filtering================
-  # titlePanel("Filtering:"),
-  # fluidRow(
-  #   column(5,
-  #          titlePanel(h4("Filtering option A:")),
-  #          numericInput(inputId = "mafA", label = "Minor Allele Frequency Minimum", value = 0, min = 0.05, max = .5),
-  #          selectInput("maf_facets_A", "MAF facet: keep any loci above specified Minor Alele Frequency in any facet level.", choices = list("Upload data to see available facets." = "none"),
-  #                      multiple = TRUE),
-  #          numericInput(inputId = "hweA", label = "HWE p-value Maximum", value = 0.0001, min = 0, max = 1),
-  #          selectInput("hwe_facets_A", "HWE facet: reject any loci out of HWE in any facet level", choices = list("Upload data to see available facets." = "none"),
-  #                      multiple = TRUE),
-  #          numericInput(inputId = "min_indA", label = "Maximum Proportion of Missing SNPs per Sample", value = 0.75, min = 0, max = 1),
-  #          numericInput(inputId = "min_lociA", label = "Maximum Proportion of Missing Samples per Locus", value = 0.75, min = 0, max = 1),
-  #          actionButton("run_filt_A", "Run?")),
-  #   column(5,
-  #          titlePanel(h4("Filtering option B:")),
-  #          numericInput(inputId = "mafB", label = "Minor Allele Frequency Minimum", value = 0),
-  #          selectInput("maf_facets_B", "MAF facet: keep any loci above specified Minor Alele Frequency in any facet level.", choices = list("Upload data to see available facets." = "none"),
-  #                      multiple = TRUE),
-  #          numericInput(inputId = "hweB", label = "HWE p-value Maximum", value = 0.000001),
-  #          selectInput("hwe_facets_B", "HWE facet: reject any loci out of HWE in any facet level", choices = list("Upload data to see available facets." = "none"),
-  #                      multiple = TRUE),
-  #          numericInput(inputId = "min_indB", label = "Maximum Proportion of Missing SNPs per Sample", value = 0.5, min = 0, max = 1),
-  #          numericInput(inputId = "min_lociB", label = "Maximum Proportion of Missing Samples per Locus", value = 0.5, min = 0, max = 1),
-  #          actionButton("run_filt_B", "Run?"))
-  # ),
-  # fluidRow(column(5,
-  #                 fluidRow(
-  #                   plotOutput("filter_validation_A"),
-  #                   textOutput("filter_validation_A_numbers")
-  #                 ),
-  #                 actionButton("filter_select_A", "Apply filtering option A")),
-  #          column(5,
-  #                 fluidRow(
-  #                   plotOutput("filter_validation_B"),
-  #                   textOutput("filter_validation_B_numbers")
-  #                 ),
-  #                 actionButton("filter_select_B", "Apply filtering option B"))
-  # ),
-  # 
-  # #===========statistics=======
-  # 
-  # 
-  # 
-  # # tests - single
-  # uiOutput(outputId = "stats_header"),
-  # checkboxGroupInput("selected_tests_single", label = "Desired Single-SNP Statistics:",
-  #                    choices = c("Ho" = "ho",
-  #                                "pi" = "pi",
-  #                                "Fst" = "Fst",
-  #                                "Fis" = "Fis",
-  #                                "HWE" = "hwe",
-  #                                "Minor Allele Frequencies" = "maf",
-  #                                "Private Alleles" = "pa")),
-  # fluidRow(checkboxInput("do_fst_boot", "Calculate Fst p-values via Bootstrapping?")),
-  # textInput("fst_boots", "# Bootstrapps", value = 1000),
-  # uiOutput("single_facets"), # provide drop down with clickable facets to use
-  # 
-  # ## tests - window
-  # checkboxGroupInput("selected_tests_window", label = "Desired Windowed Statistics",
-  #                    choices = c("Ho" = "ho",
-  #                                "pi" = "pi",
-  #                                "Fst" = "Fst",
-  #                                "Private Alleles" = "pa",
-  #                                "Tajima's D" = "tsd")),
-  # uiOutput(outputId = "window_opts"),
-  # fluidRow(textInput(inputId = "sigma", label = "Sigma (Window Size = 6*Sigma, in kb)", value = 200),
-  #          textInput(inputId = "slide", label = "Slide Between Windows (kb)", value = 50),
-  #          checkboxInput(inputId = "do_boots", "Bootstrap Window Significance?"),
-  #          textInput(inputId = "num_window_boots", "Number of Bootstraps", value = 1e6)),
-  # uiOutput("window_facets"),
-  # 
-  # ## association
-  # 
-  # ## plotting
-  # 
-  # ## tests - misc
-  # fluidRow(
-  #   uiOutput("sfs_facets"),
-  #   uiOutput("sfs_facet_levels")
-  # ),
-  # fluidRow(checkboxInput("ibd", "Isolation by Distance"),
-  #          uiOutput("snp_ibd_facets")),
-  # fluidRow(checkboxInput("ne", "Effective Population Size (Ne)"),
-  #          textInput("neestimator_path", "Path to NeEstimator executable"),
-  #          checkboxGroupInput("ne_methods", label = "Methods:", choices = list("LDNe" = "ld",
-  #                                                                              "Heterozygote Excess" = "Ht",
-  #                                                                              "Coancestry" = "coan")),
-  #          numericInput(inputId = "ne_pcrit", label = "P-crit (minimum minor allele frequency)", 0.01, 0, .5))
+  titlePanel("File Selection: "),
+  inputPanel(
+    fluidRow(column(5, h4("Fastqs:"), uiOutput("input_file_selector")),
+             column(5, h4("Barcodes:"), uiOutput("input_barcode_selector"))
+    ),
+    h4("Specify sample names?"),
+    uiOutput("demultiplex_options"),
+    uiOutput("go_demultiplex")
+  ),
 )
+
+
 
 
 
@@ -121,8 +46,9 @@ server <- function(input, output, session) {
   #==========read in input files===========
   x <- reactiveValues()
   
+  # generate image
   barcode.type <- eventReactive(input$visualize_barcodes,{
-    img_table <- data.frame(input = c("In Illumina Headers",
+    img_table <- data.frame(input = c("In Headers",
                                       "Start of Reads",
                                       "Seperate fastq File"),
                             img = c("barcode_in_header", 
@@ -134,10 +60,9 @@ server <- function(input, output, session) {
   })
   
   
-  output$barcode_locations_image<-renderUI({
-    browser()
+  output$barcode_locations_image <- renderUI({
     res <- list()
-    
+    validate(need(!all(c("Start of Reads", "In Headers") %in% input$barcodes), "Barcodes can be either in fastq headers OR at the beginning of reads, but not both!"))
     photo_size <- 300
     if(length(barcode.type()) > 1){
       photo_size <- photo_size - 50*(length(barcode.type()) - 1)
@@ -173,98 +98,169 @@ server <- function(input, output, session) {
     else{return(res)}
     
   })
-
-  # data_importer <- reactive({
-  #   x <- import_wrapper(input$genotypes, input$snp_meta, input$sample_meta, input$mDat)
-  #   valid <- validation(x)
-  #   return(list(x = x, valid = valid))
-  # })
-  # 
-  # observeEvent(input$import,{
-  #   x$data <- data_importer()
-  # })
-  # 
-  # 
-  # observeEvent(input$import | input$filter_select_A | input$filter_select_B, ignoreInit = TRUE, {
-  #   cat("updated plot")
-  #   vpns <- renderValidation(x$data$valid)
-  #   output$input_validation <- vpns$vp
-  #   output$input_validation_numbers <- vpns$vn
-  # })
-  # 
-  # 
-  # #==========filtering=====================
-  # # set up reactives
-  # filtered_sets <- reactiveValues()
-  # a <- reactive({
-  #   d <- filter_wrapper(x$data$x,
-  #                       maf = input$mafA,
-  #                       maf_facet = input$maf_facets_A,
-  #                       hwe = input$hweA,
-  #                       hwe_facet = input$hwe_facets_A,
-  #                       min_ind = input$min_indA,
-  #                       min_loci = input$min_lociA)
-  #   
-  #   v <- validation(d)
-  #   return(list(x = d, valid = v))
-  # })
-  # b <- reactive({
-  #   d <- filter_wrapper(x$data$x,
-  #                       maf = input$mafB,
-  #                       maf_facet = input$maf_facets_B,
-  #                       hwe = input$hweB,
-  #                       hwe_facet = input$hwe_facets_B,
-  #                       min_ind = input$min_indB,
-  #                       min_loci = input$min_indB)
-  #   
-  #   v <- validation(d)
-  #   return(list(x = d, valid = v))
-  # })
-  # 
-  # # facet level trackers
-  # observe({
-  #   
-  #   if(!is.null(x$data$x)){
-  #     facet_opts <- facet_check(x$data$x)
-  #     
-  #     updateSelectInput(session, "maf_facets_A", choices = facet_opts$sample)
-  #     updateSelectInput(session, "maf_facets_B", choices = facet_opts$sample)
-  #     updateSelectInput(session, "hwe_facets_A", choices = facet_opts$sample)
-  #     updateSelectInput(session, "hwe_facets_B", choices = facet_opts$sample)
-  #   }
-  #   
-  #   
-  # })
-  # 
-  # # plot filters
-  # observeEvent(input$run_filt_A,{
-  #   filtered_sets$a <- a()
-  #   av <- validation(filtered_sets$a$x)
-  #     
-  #   vpnsa <- renderValidation(av)
-  #   output$filter_validation_A <- vpnsa$vp
-  #   output$filter_validation_A_numbers <- vpnsa$vn
-  # })
-  # 
-  # observeEvent(input$run_filt_B,{
-  #   filtered_sets$b <- b()
-  #   bv <- validation(filtered_sets$b$x)
-  #   
-  #   vpnsb <- renderValidation(bv)
-  #   output$filter_validation_B <- vpnsb$vp
-  #   output$filter_validation_B_numbers <- vpnsb$vn
-  # })  
-  # 
-  # # keep a filter
-  # observeEvent(input$filter_select_A, {
-  #   x$data <- filtered_sets$a
-  # })
-  # observeEvent(input$filter_select_B, {
-  #   x$data <- filtered_sets$b
-  # })
-  # 
-  # #===================do statistics=====================
   
+  
+  
+  # generate selector boxes for both the input fastqs and the barcodes
+  ## fastqs
+  output$input_file_selector <- renderUI({
+    validate(need(!all(c("Start of Reads", "In Headers") %in% input$barcodes), ""))
+    
+    file_R_lab <- "NA"
+    fileinputs <- lapply(1:3, function(i){
+      if(i == 1){
+        file_R_lab <- "R1: read file"
+      }
+      else if(i == 2){
+        if("Seperate fastq File" %in% input$barcodes){
+          file_R_lab <- "R2: barcode file"
+        }
+        else{
+          file_R_lab <- "R2: read file"
+        }
+      }
+      else{
+        file_R_lab <- "R3: read file"
+      }
+      shinyFilesButton(paste0("fastq_R", i), label = file_R_lab, multiple = FALSE, accept = c(".fastq", ".fq"), title = ".fastq or .fq file.")
+    })
+    
+    shinyjs::hidden(fileinputs)
+  })
+  shinyFileChoose(input, 'fastq_R1', root=c(root=root))
+  shinyFileChoose(input, 'fastq_R2', root=c(root=root))
+  shinyFileChoose(input, 'fastq_R3', root=c(root=root))
+  
+  
+  ## barcodes
+  output$input_barcode_selector <- renderUI({
+    validate(need(!all(c("Start of Reads", "In Headers") %in% input$barcodes), ""))
+    
+    barcode_file_lab_index <- "NA"
+    barcodeinputs <- lapply(1:2, function(i){
+      if(i == 1){
+        if("In Headers" %in% input$barcodes){
+          barcode_file_lab_index <- "header barcodes"
+        }
+        else if("Start of Reads" %in% input$barcodes){
+          barcode_file_lab_index <- "in-read barcodes"
+        }
+        else{
+          barcode_file_lab_index <- "R2 read barcodes"
+        }
+      }
+      else{
+        barcode_file_lab_index <- "R2 read barcodes"
+      }
+      shinyFilesButton(paste0("barcode_file_", i), label = barcode_file_lab_index, multiple = FALSE, title = "File with barcodes/indices, one per line.")
+    })
+    
+    shinyjs::hidden(barcodeinputs)
+  })
+  shinyFileChoose(input, 'barcode_file_1', root=c(root=root))
+  shinyFileChoose(input, 'barcode_file_2', root=c(root=root))
+  
+  
+  
+  observeEvent(eventExpr = input$visualize_barcodes, handlerExpr = {
+    if(length(input$barcodes) != 0){
+      shinyjs::show("fastq_R1")
+    }
+    
+    
+    if("In Headers" %in% input$barcodes | "Start of Reads" %in% input$barcodes){
+      shinyjs::show("barcode_file_1")
+    }
+    
+    if(input$is.paired){
+      shinyjs::show("fastq_R2")
+      
+      if("Seperate fastq File" %in% input$barcodes){
+        shinyjs::show("fastq_R3")
+        shinyjs::show("barcode_file_2")
+      }
+    }
+    else if("Seperate fastq File" %in% input$barcodes){
+      shinyjs::show("fastq_R2")
+      shinyjs::show("barcode_file_2")
+    }
+  })
+  
+
+  output$demultiplex_options <- renderUI({
+    sids <- fileInput("sample_ids", "Optional Sample Identifications", placeholder = "Tab delimited key for demultiplexed samples. Two or three columns for: seperate fastq file barcodes, header/in-read barcodes, and sample name.")
+    shinyjs::hidden(sids)
+  })
+  output$go_demultiplex <- renderUI({
+    gdb <- actionButton("go_demultiplex", "Demultiplex reads!")
+    shinyjs::hidden(gdb)
+  })
+
+  
+  # locate appropriate files
+  observeEvent(eventExpr = 
+                 is.list(input$fastq_R1) | 
+                 is.list(input$fastq_R2) | 
+                 is.list(input$fastq_R3) | 
+                 is.list(input$barcode_file_1) | 
+                 is.list(input$barcode_file_2), ignoreInit = TRUE, handlerExpr = {
+    display <- FALSE
+    files <- list()
+
+    # read files for every case:
+    ## with in-line or header barcodes
+    if(("In Headers" %in% input$barcodes | "Start of Reads" %in% input$barcodes) & is.list(input$barcode_file_1) & is.list(input$fastq_R1)){
+      browser()
+      
+      # not paired, dual indexed
+      if(!input$is.paired & "Seperate fastq File" %in% input$barcodes & is.list(input$fastq_R2) & is.list(input$barcode_file_2)){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$barcode_file_2 <- .parse_shinyFiles_path(root, input$barcode_file_2)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+        files$R2 <- .parse_shinyFiles_path(root, input$fastq_R2)
+      }
+      
+      # paired, dual indexed
+      else if(input$is.paired & "Seperate fastq File" %in% input$barcodes & is.list(input$fastq_R2) & is.list(input$fastq_R3) & is.list(input$barcode_file_2)){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$barcode_file_2 <- .parse_shinyFiles_path(root, input$barcode_file_2)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+        files$R2 <- .parse_shinyFiles_path(root, input$fastq_R2)
+        files$R3 <- .parse_shinyFiles_path(root, input$fastq_R3)
+      }
+      
+      # not paired, single indexed
+      else if(!input$is.paired & !"Seperate fastq File" %in% input$barcodes){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+      }
+      
+      else if(input$is.paired & !"Seperate fastq File" %in% input$barcodes & is.list(input$fastq_R2)){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+        files$R3 <- .parse_shinyFiles_path(root, input$fastq_R2) # named R3 for consistancy later
+      }
+    }
+    
+    ## with only R2 barcodes
+    else if(!("In Headers" %in% input$barcodes | "Start of Reads" %in% input$barcodes) & is.list(input$barcode_file_1) & is.list(input$fastq_R1)){
+      # not paired
+      browser()
+      if(!input$is.paired){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+        files$R2 <- .parse_shinyFiles_path(root, input$fastq_R2)
+      }
+      
+      # paired
+      if(input$is.paired & is.list(input$fastq_R2)){
+        files$barcode_file_1 <- .parse_shinyFiles_path(root, input$barcode_file_1)
+        files$R1 <- .parse_shinyFiles_path(root, input$fastq_R1)
+        files$R3 <- .parse_shinyFiles_path(root, input$fastq_R2)
+      }
+    }
+    
+  })
 } 
 
 shinyApp(ui, server)
